cde5ded1ce14881af93ddd2ee6211e36bddd15be
Dinesh Chohda <dinesh.skyach@gmail.com>
2018-02-08 18:50:58 +0530

    removed unused js and css files

--- app/assets/javascripts/admin.js
+++
@@ -1,8 +0,0 @@
-//= require yarn_components/raven-js/dist/raven
-//= require ./lib/sentry
-
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-//= require bootstrap-datetimepicker
-//= require admin/app
